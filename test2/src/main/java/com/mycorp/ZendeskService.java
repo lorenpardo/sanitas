88ec30ad604b2b5fa0e59358ea9a7f896626abcd
lorenpardo <lorenpardo@gmail.com>
2019-03-27 15:30:59 +0100

    prueba

--- test2/src/main/java/com/mycorp/ZendeskService.java
+++ test2/src/main/java/com/mycorp/ZendeskService.java
@@ -1,5 +1,6 @@
 package com.mycorp;
 
+
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
